b5cb6c4bd19369062cafe24885315a26e06aee03
KingsDevs <sunnyckair@gmail.com>
2020-12-02 23:18:40 +0800

    added clunky visualizer

--- venv/main.py
+++ venv/main.py
@@ -11,10 +11,10 @@ BACKGROUND_COLOR = (53,45,49)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-num_bars = 10
+num_bars = 40
 bar_width = 20
 space = 5
-delay = .1
+delay = .01
 
 
 pygame.init()
@@ -41,18 +41,14 @@ def draw_button(title,x,y,w,h,ic,ac):
     screen.blit(text, (x+10, y+10))
     return sorting
 
-so = sorting_algo(screen,BACKGROUND_COLOR,BLACK,num_bars,bar_width,space,WIDTH,delay)
-
+so = sorting_algo(screen,BACKGROUND_COLOR,BLACK,WHITE,num_bars,bar_width,space,WIDTH,delay)
+arr = so.random_items()
 while True:
-    sorting = False
-    random_array = draw_button("Random", 300-75/2,200-25,110,50,(230,230,230),(200,200,200))
-    pygame.display.update()
-    if random_array:
-        arr = so.random_items()
-        sorting = draw_button("Sort", 200 - 75 / 2, 200 - 25, 75, 50, (230, 230, 230), (200, 200, 200))
+    sorting = draw_button("Sort", 200 - 75 / 2, 200 - 25, 75, 50, (230, 230, 230), (200, 200, 200))
     if sorting:
-        print(so.bubble_sort(arr))
-
+        arr = so.odd_even_sort(arr)
+        so.drawbars(arr,BLACK)
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
--- venv/sorting_algo.py
+++ venv/sorting_algo.py
@@ -5,10 +5,11 @@ import random
 
 class sorting_algo:
 
-    def __init__(self,screen,backcolor,color,num_bars,bar_width,space,width,delay):
+    def __init__(self,screen,backcolor,color,act_color,num_bars,bar_width,space,width,delay):
         self.screen = screen
         self.backcolor = backcolor
         self.color = color
+        self.act_color = act_color
         self.num_bars = num_bars
         self.bar_width = bar_width
         self.space = space
@@ -23,64 +24,58 @@ class sorting_algo:
     def random_items(self):
         arr = []
         for i in range(self.num_bars):
-            height = random.randint(-100, -1)
+            height = random.randint(1, 100)
             arr.append(height)
             x = self.x_bar(i)
-            self.draw_bar(height,x)
+            self.draw_bar(height,x,self.color)
         return arr
 
-    def drawbars(self,arr):
+    def drawbars(self,arr,color):
         for i in range(len(arr)):
             x = self.x_bar(i)
-            self.draw_bar(arr[i],x)
+            self.draw_bar(arr[i],x,color)
+            time.sleep(self.delay)
+            pygame.display.update()
 
-    def draw_bar(self,height,x):
-        pygame.draw.rect(self.screen, self.color, (x, 400, self.bar_width, height), 0)
+
+    def draw_bar(self,height,x,color):
+        pygame.draw.rect(self.screen, color, (x, 400, self.bar_width, height), 0)
 
     def selection_sort(self,a):
-        elapsed = self.clock.tick(30)
+        elapsed = self.clock.tick(1)
         counter = 0
         minimum_index = counter
         traversing_index = counter + 1
         while(counter < len(a)):
+            color = self.color
+            self.screen.fill(self.backcolor)
 
             for traversing_index in range(counter, len(a)):
                 if a[traversing_index] < a[minimum_index]:
                     minimum_index = traversing_index
                 traversing_index += 1
-            for i in range(len(a)):
-                x = self.x_bar(i)
-                height = a[i]
-                self.draw_bar(height,x)
-                time.sleep(self.delay)
-                pygame.display.update()
-            self.screen.fill(self.backcolor)
+            self.drawbars(a, self.color)
             a[counter], a[minimum_index] = a[minimum_index], a[counter]
             counter += 1
             minimum_index = counter
             traversing_index = counter + 1
+        self.drawbars(a, self.color)
         return a
 
     def bubble_sort(self,a):
         swapped_values = True
         traversing_index = 0
-
+        elapsed = self.clock.tick(1)
         while(swapped_values):
             swapped_values = False
             traversing_index = 0
-            for i in range(len(a)):
-                x = self.x_bar(i)
-                height = a[i]
-                self.draw_bar(height,x)
-                time.sleep(self.delay)
-                pygame.display.update()
+            self.drawbars(a,self.color)
             for traversing_index in range(len(a)-1):
                 if a[traversing_index] > a[traversing_index + 1]:
                     a[traversing_index], a[traversing_index + 1] = a[traversing_index + 1], a[traversing_index]
                     swapped_values = True
-
+            self.screen.fill(self.backcolor)
         return a
-
 
     def cocktail_shaker_sort(self,a):
         swapped_values = True
@@ -101,6 +96,8 @@ class sorting_algo:
                         a[i], a[i - 1] = a[i - 1], a[i]
                         swapped_values = True
                     traversing_index -= 1
+            self.drawbars(a,self.color)
+            self.screen.fill(self.backcolor)
 
         return a
 
@@ -118,6 +115,10 @@ class sorting_algo:
                 if a[i] > a[i+1]:
                     a[i], a[i+1] = a[i+1], a[i]
                     is_sorted = False
+            self.drawbars(a,self.color)
+            self.screen.fill(self.backcolor)
+
+
 
         return a
 
